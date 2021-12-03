dfdac5f49806498418b8853c4428c10accf7cf52
ndetis elie <nyobedjobelie@gmail.com>
2021-12-03 22:43:19 +0000

    Creation du menu

--- src/GestEtudiant.java
+++ src/GestEtudiant.java
@@ -2,30 +2,68 @@ import java.util.Scanner;
 
 public class GestEtudiant {
 
-    public static void gestionEtudiant(){
+    public static void gestionEtudiant() {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Entre l'effective de la salle");
         int NumEtudiant = scanner.nextInt();
-        Etudiant Tab[] = new Etudiant [NumEtudiant];
-        for(int i =0 ; i < NumEtudiant ; i++){
+        Etudiant Tab[] = new Etudiant[NumEtudiant];
+        for (int i = 0; i < NumEtudiant; i++) {
             Tab[i] = CreateEtudiant.createEtudiant();
         }
 
         Etudiant Tmp;
-        for (int i =0 ; i < NumEtudiant-1 ; i++){
-          for (int j = i+1 ; j < NumEtudiant ;  j++) {
-                if (Tab[i].getMoyenne() > Tab[j].getMoyenne() ){
+        for (int i = 0; i < NumEtudiant - 1; i++) {
+            for (int j = i + 1; j < NumEtudiant; j++) {
+                if (Tab[i].getMoyenne() > Tab[j].getMoyenne()) {
                     Tmp = Tab[i];
                     Tab[i] = Tab[j];
-                    Tab[j] = Tmp ;
+                    Tab[j] = Tmp;
                 }
-          }
+            }
         }
         // Menu Principal
-        for (int i =0 ; i < NumEtudiant ; i++){
-            Tab[i].afficher();
+
+
+            menu :
+            System.out.println("\n ------ MENU DE GESTION D'ETUDIANT ------ \n");
+            System.out.println(" 1. Afficher les information des etudiant \n");
+            System.out.println(" 2. Afficher l'information du premiere etudiant \n");
+            System.out.println(" 3. Afficher l'information du dernier etudiant \n");
+            System.out.println(" 4. Reinitialiser la liste de la classe \n");
+            System.out.println(" 5. Sortir du programme \n");
+
+            System.out.println("Votre choix : ");
+            int choice = scanner.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < NumEtudiant; i++) {
+                        Tab[i].afficher();
+                    }
+                    break;
+                case 2:
+                    Tab[0].afficher();
+                    break;
+                case 3:
+                    Tab[NumEtudiant - 1].afficher();
+                    break;
+                case 4:
+                    char choix = 'A';
+                    System.out.println("voulez vous reinitialiser la liste ? \n o(oui) ... \n n(non) ");
+                    choix = scanner.nextLine().charAt(0);
+                    if (choix == 'n') {
+                        System.out.println("Merci votre demande d'annulation a ete fait");
+                        break;
+                    }
+                    if (choix == 'o') {
+                        break;
+                    }
+                case 5:
+                    System.out.println("Fin du programme");
+                    break;
+            }
         }
-    }
 }
